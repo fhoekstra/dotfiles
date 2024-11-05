7d27526babcfe9afee5fb4a5670ddc5600ecbc22
Freek Hoekstra <freek.hoekstra@rivm.nl>
2024-11-05 21:06:24 +0100

    Adapt neovim dotfiles to Nix-managed lazyvim install

---
+++ dot_config/nvim/lua/plugins/extras_other.lua
@@ -0,0 +1,6 @@
+return {
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.refactoring" },
+  { import = "lazyvim.plugins.extras.editor.telescope" },
+  { import = "lazyvim.plugins.extras.util.chezmoi" },
+}
