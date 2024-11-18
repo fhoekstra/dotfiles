8c5c7a638b1fffa7df1353fe366c910b74410eb4
Freek Hoekstra <freek.hoekstra@rivm.nl>
2024-11-18 15:07:57 +0100

    nvim: Add word wrap options and keymap stub

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- Set word wrap at edge of window
+vim.opt.wrap = true
+vim.opt.textwidth = 0
+vim.opt.wrapmargin = 0
+vim.opt.linebreak = true -- (optional - breaks by word rather than character)
+
+-- set clipboard
+-- vim.opt.clipboard = "unnamedplus"
