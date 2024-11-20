07adb12ce63b05ea41582fb0c72edf28f55b5cb3
Freek <freek.george.hoekstra@gmail.com>
2024-11-20 20:56:37 +0100

    nix: Make machine specific template smaller, split hosts among multiple files that are always shipped

---
+++ dot_config/home-manager/hosts/default.nix
@@ -0,0 +1,14 @@
+{
+  username = "freek";
+  homeDirectory = "/home/freek";
+  theme = "tokyo-night";
+  # TODO: themes to support:
+  # - tokyo-night
+  # - gruvbox
+  # - light
+  # - dracula
+  # these themes should be automatically applied to:
+  # - nvim
+  # - tmux
+  # - zsh
+}
---
+++ dot_config/home-manager/hosts/fedora-laptop.nix
@@ -0,0 +1,5 @@
+{
+  username = "freek";
+  homeDirectory = "/home/freek";
+  theme = "light";
+}
