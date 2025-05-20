734a65489bfc4c8ea0132bf3225a0045afa83854
Freek <freekgeorgehoekstra@gmail.com>
2025-05-20 20:49:06 +0200

    Update .config/home-manager/hosts/desktop.nix

--- dot_config/home-manager/hosts/desktop.nix
+++ dot_config/home-manager/hosts/desktop.nix
@@ -5,7 +5,15 @@
   ...
 }:
 {
-  # theme = "light";
+  services.home-manager.autoExpire = {
+    enable = true;
+    timestamp = "-7 days";
+    frequency = "weekly";
+    store = {
+      cleanup = true;
+      options = "--delete-older-than 30d";
+    };
+  };
   home.packages = [ pkgs.mise ];
   programs.zsh.initExtra = ''
     eval "$(/usr/bin/mise activate zsh)"
