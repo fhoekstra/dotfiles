ebd5b2a057981a2119c34043c05118eb41ae0075
Freek <dev@freekhoekstra.nl>
2025-06-08 18:11:14 +0200

    Expand desktop cachyos setup

--- dot_config/home-manager/hosts/desktop.nix
+++ dot_config/home-manager/hosts/desktop.nix
@@ -5,8 +5,27 @@
   ...
 }:
 {
-  home.packages = [ pkgs.mise ];
+  home.packages = [
+    pkgs.mise
+    pkgs.cosign
+  ];
   programs.zsh.initContent = ''
-    eval "$(/usr/bin/mise activate zsh)"
+    eval "$(mise activate zsh)"
+    export PATH=$PATH:~/.local/bin/
   '';
+  home.shellAliases = {
+    rmpkg = "sudo pacman -Rsn";
+    cleanch = "sudo pacman -Scc";
+    fixpacman = "sudo rm /var/lib/pacman/db.lck";
+    update = "sudo pacman -Syu";
+    cleanup = "sudo pacman -Rsn $(pacman -Qtdq)";
+  };
+  home.sessionVariables = {
+    HISTCONTROL = "ignoreboth";
+    HISTIGNORE = "&:[bf]g:c:clear:history:exit:q:pwd:* --help";
+    LESS_TERMCAP_md = "$(tput bold 2> /dev/null; tput setaf 2 2> /dev/null)";
+    LESS_TERMCAP_me = "$(tput sgr0 2> /dev/null)";
+    PROMPT_COMMAND = "history -a; $PROMPT_COMMAND";
+  };
+
 }
