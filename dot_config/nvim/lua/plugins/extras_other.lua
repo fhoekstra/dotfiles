11bb7345736943e9b2a4e895eafae0523e531c99
Freek <freek.george.hoekstra@gmail.com>
2025-06-30 22:05:28 +0200

    feat!: Use native neovim package and let lazyvim manage nvim plugins

--- dot_config/nvim/lua/plugins/extras_other.lua
+++
@@ -1,6 +0,0 @@
-return {
-  { import = "lazyvim.plugins.extras.coding.mini-surround" },
-  { import = "lazyvim.plugins.extras.editor.refactoring" },
-  { import = "lazyvim.plugins.extras.editor.telescope" },
-  { import = "lazyvim.plugins.extras.util.chezmoi" },
-}
