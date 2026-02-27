60dfdeed2b2e7fca471c0995f041fbf214108369
Freek <freek.george.hoekstra@gmail.com>
2026-02-27 14:25:00 +0100

    feat(nvim): Add binding to add link in Markdown

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -10,3 +10,27 @@ map("n", "<leader>lz", "<cmd>Lazy<cr>", { desc = "Lazy" })
 map("n", "<leader>lg", function()
   Snacks.lazygit({ cwd = LazyVim.root.git() })
 end, { desc = "Lazygit (Root Dir)" })
+
+local function wrap_as_markdown_link()
+  local url = vim.fn.getreg("+")
+  local start = vim.fn.getpos("v")
+  local finish = vim.fn.getpos(".")
+  local lines = vim.fn.getregion(start, finish, { type = "v" })
+  local selected = table.concat(lines, "\n")
+
+  vim.cmd("normal! d")
+  vim.fn.setreg("+", url)
+  local col = vim.fn.col(".")
+  local line = vim.fn.line(".")
+  vim.api.nvim_buf_set_text(0, line - 1, col - 1, line - 1, col - 1, { "[" .. selected .. "](" .. url .. ")" })
+end
+
+vim.keymap.set("v", "<leader>ml", function()
+  wrap_as_markdown_link()
+  vim.cmd("startinsert")
+end, { desc = "Wrap as markdown link" })
+
+vim.keymap.set("n", "<leader>ml", function()
+  local esc = vim.api.nvim_replace_termcodes("viw<leader>ml", true, false, true)
+  vim.api.nvim_feedkeys(esc, "x", false)
+end, { desc = "Wrap word as markdown link" })
