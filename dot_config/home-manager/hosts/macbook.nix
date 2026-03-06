d40f2cd7b33013045643ecf13ab86a77e03ba987
Freek Hoekstra <git@freekhoekstra.nl>
2026-03-06 13:34:54 +0100

    chore(hosts/macbook): Add some work-specific packages

--- dot_config/home-manager/hosts/macbook.nix
+++ dot_config/home-manager/hosts/macbook.nix
@@ -9,12 +9,17 @@ let
 in
 {
   home.packages = [
-    pkgs.joplin
+    pkgs.joplin # notes
+    pkgs.azure-cli
+    pkgs.kubelogin # Azure Kubernetes login
   ];
   home.username = username;
   home.homeDirectory = "/Users/${username}";
 
   programs.zsh.initContent = ''
     ssh-add --apple-load-keychain -q
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+    [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
   '';
 }
