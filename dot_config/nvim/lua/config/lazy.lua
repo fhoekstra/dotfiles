1265a8f947bc521d01d475a26ecc7e813243e88c
Freek <dev@freekhoekstra.nl>
2026-03-08 20:23:09 +0100

    feat(nvim): Add C ls and formatter

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.docker" },
--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -3,6 +3,7 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
+      c = { "clang-format" },
       nix = { "nixfmt" },
       ansible = { "prettier" },
       yaml = { "prettier" },
