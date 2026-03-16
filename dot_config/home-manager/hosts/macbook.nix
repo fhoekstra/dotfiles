b4ab33df03e6c40d869a7676ada96279eb0d29f5
fhoekstra <git@freekhoekstra.nl>
2026-03-16 11:48:43 +0100

    chore(mac): Add alias for nx

--- dot_config/home-manager/hosts/macbook.nix
+++ dot_config/home-manager/hosts/macbook.nix
@@ -21,5 +21,6 @@ in
     export NVM_DIR="$HOME/.nvm"
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+    alias nx='npx nx'
   '';
 }
