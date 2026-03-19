2a7fb53723fc98903740a76fc535936d5b75fd85
fhoekstra <git@freekhoekstra.nl>
2026-03-19 10:22:54 +0100

    fix(nvim): Add auto-format and auto-organize imports for web frontend languages

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,3 +1,6 @@
+local util = require("conform.util")
+local ts_js_css_html_formatters = { "biome", "biome_fix", "biome-organize-imports" }
+local yaml_formatters = { "prettier" }
 return {
   "stevearc/conform.nvim",
   optional = true,
@@ -5,15 +8,24 @@ return {
     formatters_by_ft = {
       c = { "clang-format" },
       nix = { "nixfmt" },
-      ansible = { "prettier" },
-      yaml = { "prettier" },
-      json = { "biome" },
-      javascript = { "biome" },
-      javascriptreact = { "biome" },
-      typescript = { "biome" },
-      typescriptreact = { "biome" },
-      html = { "biome" },
-      css = { "biome" },
+      ansible = yaml_formatters,
+      yaml = yaml_formatters,
+      json = ts_js_css_html_formatters,
+      javascript = ts_js_css_html_formatters,
+      javascriptreact = ts_js_css_html_formatters,
+      typescript = ts_js_css_html_formatters,
+      typescriptreact = ts_js_css_html_formatters,
+      html = ts_js_css_html_formatters,
+      css = ts_js_css_html_formatters,
+    },
+    formatters = {
+      biome_fix = {
+        command = "node_modules/.bin/biome",
+        args = { "check", "--write", "--stdin-file-path", "$FILENAME" },
+        stdin = true,
+        cwd = util.root_file({ "biome.json", "biome.jsonc" }),
+        require_cwd = true,
+      },
     },
   },
 }
