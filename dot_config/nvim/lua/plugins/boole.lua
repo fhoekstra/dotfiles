ec1b655cc5d57a495e8a0313f73d55d5e637e3b2
Freek <freek.george.hoekstra@gmail.com>
2026-03-28 20:09:51 +0100

    feat(nvim): Add boole.nvim for toggling booleans (and more)

---
+++ dot_config/nvim/lua/plugins/boole.lua
@@ -0,0 +1,14 @@
+return {
+  "nat-418/boole.nvim",
+  opts = {
+    mappings = {
+      increment = "<C-a>",
+      decrement = "<C-x>",
+    },
+    additions = {
+      -- Example additions (can be customized further)
+      -- { "foo", "bar" },
+      -- { "tic", "tac", "toe" },
+    },
+  },
+}
