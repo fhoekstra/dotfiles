03f1281e299e6390835b7adc43162db9476f326f
fhoekstra <git@freekhoekstra.nl>
2026-03-30 17:53:33 +0200

    feat(mac): Add nb alias for netbird

--- dot_config/home-manager/hosts/macbook.nix
+++ dot_config/home-manager/hosts/macbook.nix
@@ -19,11 +19,14 @@ in
 
   home.sessionVariables.K9S_CONFIG_DIR = "${homeDir}/.config/k9s";
 
+  home.shellAliases = {
+    nx = "npx nx";
+    nb = "netbird";
+  };
   programs.zsh.initContent = ''
     ssh-add --apple-load-keychain -q
     export NVM_DIR="$HOME/.nvm"
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-    alias nx='npx nx'
   '';
 }
