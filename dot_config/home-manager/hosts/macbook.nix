72e19681ac0083cd83d2ce4b30445d1a1e34ff61
fhoekstra <git@freekhoekstra.nl>
2026-04-02 11:39:54 +0200

    feat(mac): Load aikido safe-chain

--- dot_config/home-manager/hosts/macbook.nix
+++ dot_config/home-manager/hosts/macbook.nix
@@ -25,8 +25,11 @@ in
   };
   programs.zsh.initContent = ''
     ssh-add --apple-load-keychain -q
+
     export NVM_DIR="$HOME/.nvm"
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+    source ~/.safe-chain/scripts/init-posix.sh # Aikido safe-chain
   '';
 }
