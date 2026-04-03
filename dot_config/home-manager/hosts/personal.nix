09651c8d49507fd35c6e286162178ab4ec804b8d
Freek <freek.george.hoekstra@gmail.com>
2026-04-03 19:02:32 +0200

    feat(nix): Add Mistral API key via hosts/personal.nix

--- dot_config/home-manager/hosts/personal.nix
+++ dot_config/home-manager/hosts/personal.nix
@@ -4,8 +4,16 @@
   lib,
   ...
 }:
+let
+  secrets = if builtins.pathExists ../local-secrets.nix then (import ../local-secrets.nix) else { };
+in
 {
   home.packages = [
     pkgs.joplin
   ];
+  home.sessionVariables = {
+    # LC_ALL = "C.UTF-8";
+    EDITOR = "nvim";
+    MISTRAL_API_KEY = secrets.MISTRAL_API_KEY or "";
+  };
 }
