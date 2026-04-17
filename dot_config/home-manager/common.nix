b8965746597de0f4eb604974f076ff569551b15a
Freek <freek.george.hoekstra@gmail.com>
2026-04-17 20:10:23 +0200

    feat(nix): Add opencode

--- dot_config/home-manager/common.nix
+++ dot_config/home-manager/common.nix
@@ -129,6 +129,9 @@ in
     pkgs.gh
     pkgs.git-cliff # Semver bump and changelog automation
 
+    # AI
+    pkgs.opencode
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
