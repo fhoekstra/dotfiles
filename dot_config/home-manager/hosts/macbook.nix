df5d18c75af52223aaf125ad6c08c8a5d4056dfd
fhoekstra <git@freekhoekstra.nl>
2026-04-29 19:01:07 +0200

    feat(mac): Add colima-specific env vars
    Colima is a lightweight VM for running containers on macOS. It has
    Docker client compatibility and a built-in k3s for local Kubernetes
    development

--- dot_config/home-manager/hosts/macbook.nix
+++ dot_config/home-manager/hosts/macbook.nix
@@ -7,6 +7,13 @@
 let
   username = "freekhoekstra";
   homeDir = "/Users/${username}";
+  HOME = homeDir;
+  colima_env_vars = ''
+    export DOCKER_HOST="unix://${HOME}/.colima/docker.sock"
+    export TESTCONTAINERS_HOST_OVERRIDE=127.0.0.1
+    export TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE=/var/run/docker.sock
+    export TESTCONTAINERS_RYUK_DISABLED=true
+  '';
 in
 {
   home.packages = [
@@ -29,6 +36,10 @@ in
     export NVM_DIR="$HOME/.nvm"
     [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
     [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+    ${colima_env_vars}
+
+    eval "$(mise activate zsh)"
 
     source ~/.safe-chain/scripts/init-posix.sh # Aikido safe-chain
   '';
