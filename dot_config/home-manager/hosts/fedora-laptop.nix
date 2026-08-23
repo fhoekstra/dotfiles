531b8661e7d4c60b9262104fdb7a330bf2d5946c
Freek <freek.george.hoekstra@gmail.com>
2026-08-23 10:05:07 +0200

    chore(fedora): Add cargo path

--- dot_config/home-manager/hosts/fedora-laptop.nix
+++ dot_config/home-manager/hosts/fedora-laptop.nix
@@ -10,6 +10,7 @@
   ];
   # theme = "light";
   programs.zsh.initContent = ''
+    PATH="$PATH:/home/freek/.cargo/bin"
     eval "$(/home/freek/.local/bin/mise activate zsh)"
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
   '';
